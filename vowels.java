99af87f517c7381fd5d37d29edb52ae2cd8b63c7
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2019-10-07 14:27:42 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class vowels
+{
+public static void main(String args[])
+{
+int one=0;
+int two=0;
+int three=0;
+int four=0;
+int five=0;
+String message="hrithik";
+message=message.toLowerCase();
+for(int i=0; i<message.length();i++)
+{
+char wi=messae.charAt(i);
+if (wi=='a')
+{
+one++;
+}
+if (wi='e')
+{
+two++;
+}
+if (wi=='i')
+{
+three++;
+}
+if (wi="i")
+{
+four++;
+}
+if (wi="u")
+{
+five++;
+}
+}
+System.out.println("a"+one);
+System.out.println("e"+two);
+System.out.println("i"+three);
+System.out.println("o"+four);
+System.out.println("u"+five);
+}
+}
+
